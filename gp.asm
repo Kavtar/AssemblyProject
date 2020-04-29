58454631027d10ffa7cd19c026fae3c589d0cdaf
ZurabKavtaradze <ZurabKavtaradze@protonmail.com>
2020-04-29 11:56:02 +0400

    add validation and messages

--- gp.asm
+++ gp.asm
@@ -3,6 +3,8 @@
 
 .data
 	prompt: .asciiz "Enter size: "
+	messageForValid: .asciiz "Input is valid!"
+	messageForInvalid: .asciiz "Input is invalid, size matters!"
 .text
 
 	# prompt user to enter size
@@ -16,3 +18,17 @@
 	
 	# store result in $t0
 	move $t0, $v0
+	
+	bgt $t0, 2, InputIsValid
+	# if input is less or equal to two (e.i. invalid)
+	li $v0, 4
+	la $a0, messageForInvalid
+	syscall
+	# terminate program
+	li $v0, 10
+	syscall
+	
+	InputIsValid:
+	li $v0, 4
+	la $a0, messageForValid
+	syscall
