d059e5494be5eabe51ee0fcff9230235820dbc3c
ZurabKavtaradze <ZurabKavtaradze@protonmail.com>
2020-04-29 11:21:45 +0400

    add prompt message to the user

--- gp.asm
+++ gp.asm
@@ -1,2 +1,11 @@
 #TODO: Whole fucking project
 #TODO: Hope this is the last project in MIPS
+
+.data
+	prompt: .asciiz "Enter size: "
+.text
+
+	# prompt user to enter size
+	li $v0, 4
+	la $a0, prompt
+	syscall
