cc1e6d3c427920aba6fe4f6c205b965f2c017428
DimitriDondoladze <DimitriDondoladze@protonmail.com>
2020-05-05 20:14:30 +0400

    add minor changes

--- gp.asm
+++ gp.asm
@@ -1,12 +1,8 @@
-#TODO: Whole fucking project
-#TODO: Hope this is the last project in MIPS
-
 .data
 	prompt: .asciiz "Enter size: "
-	messageForValid: .asciiz "Input is valid!\n"
 	messageForInvalid: .asciiz "Input is invalid, size matters!"
 	comma: .asciiz ","
-	space:.asciiz  " "          # space to insert between numbers
+	space:.asciiz  " "           # space to insert between numbers
 	head: .asciiz  "The Fibonacci numbers are:\n"
 	fibs: .word   0 : 50         # "array" of words to contain fib values
 	fileName: .asciiz "D:\logs.txt"
@@ -71,8 +67,8 @@
 	#---------------------------------------------#
 	
 	# close file
-	li $v0, 16		# close file, syscall code = 16
-	move $a0, $s1		# file descriptor
+	li $v0, 16				# close file, syscall code = 16
+	move $a0, $s1				# file descriptor
 	syscall
 	
 	#---------------------------------------------#
@@ -87,61 +83,56 @@
 	la $a1, validInputMessageLogging	# string which will be written
 	la $a2, 32				# specify string length
 	syscall
-	
-	li $v0, 4
-	la $a0, messageForValid
-	syscall
-	
-	
-	la   $s0, fibs        # load address of array
+
+		
+	la   $s0, fibs        			# load address of array
     
-	
-	li   $s2, 1           # 1 is the known value of first and second Fib. number
-      	sw   $s2, 0($s0)      # F[0] = 1
-      	sw   $s2, 4($s0)      # F[1] = F[0] = 1
-      	addi $s7, $t0, -2     # Counter for loop, will execute (size-2) times
+	li   $s2, 1           			# 1 is the known value of first and second Fib. number
+      	sw   $s2, 0($s0)      			# F[0] = 1
+      	sw   $s2, 4($s0)      			# F[1] = F[0] = 1
+      	addi $s7, $t0, -2     			# Counter for loop, will execute (size-2) times
       
       	# Loop to compute each Fibonacci number using the previous two Fib. numbers.
-loop: 	lw   $s3, 0($s0)      # Get value from array F[n-2]
-      	lw   $s4, 4($s0)      # Get value from array F[n-1]
+loop: 	lw   $s3, 0($s0)      			# Get value from array F[n-2]
+      	lw   $s4, 4($s0)      			# Get value from array F[n-1]
       	#######
       	addi $t7, $zero, 1073741823
       	blt $s3, $t7, BranchOnNotOverflow
       	# write into file
-	li $v0, 15				# write file syscall code = 15
-	move $a0, $s1				# file descriptor
-	la $a1, overflowMessageForLogging	# string which will be written
-	la $a2, 64				# specify string length
+	li $v0, 15					# write file syscall code = 15
+	move $a0, $s1					# file descriptor
+	la $a1, overflowMessageForLogging		# string which will be written
+	la $a2, 64					# specify string length
 	syscall
 	# write into file
 	li $v0, 15					# write file syscall code = 15
 	move $a0, $s1					# file descriptor
 	la $a1, overflowTerminateMessageForLogging	# string which will be written
-	la $a2, 26					# specify string length
+	la $a2, 28			        	# specify string length
 	syscall
 	# close file
-	li $v0, 16		# close file, syscall code = 16
-	move $a0, $s1		# file descriptor
+	li $v0, 16					# close file, syscall code = 16
+	move $a0, $s1					# file descriptor
 	syscall
 	
 	#---------------------------------------------#
 	
 	
       	# The program is finished. Exit.
-      	li   $v0, 10          # system call for exit
-      	syscall               # Exit!
+      	li   $v0, 10          			 # system call for exit
+      	syscall             			 # Exit!
       	############
       	BranchOnNotOverflow:
-      	add  $s2, $s3, $s4    # F[n] = F[n-1] + F[n-2]
-      	sw   $s2, 8($s0)      # Store newly computed F[n] in array
-      	addi $s0, $s0, 4      # increment address to now-known Fib. number storage
-      	addi $s7, $s7, -1     # decrement loop counter
-      	bgtz $s7, loop        # repeat while not finished
+      	add  $s2, $s3, $s4    			 # F[n] = F[n-1] + F[n-2]
+      	sw   $s2, 8($s0)    			 # Store newly computed F[n] in array
+      	addi $s0, $s0, 4     			 # increment address to now-known Fib. number storage
+      	addi $s7, $s7, -1     			 # decrement loop counter
+      	bgtz $s7, loop       			 # repeat while not finished
       
       	# Fibonacci numbers are computed and stored in array. Print them.
-      	la   $a0, fibs        # first argument for print (array)
-      	add  $a1, $zero, $t0  # second argument for print (size)
-      	jal  PrintArray            # call print routine. 
+      	la   $a0, fibs       			 # first argument for print (array)
+      	add  $a1, $zero, $t0  			 # second argument for print (size)
+      	jal  PrintArray           		 # call print routine. 
 
 
 	#---------------------------------------------#
@@ -149,21 +140,21 @@ loop: 	lw   $s3, 0($s0)      # Get value from array F[n-2]
 	# write into file
 	li $v0, 15				# write file syscall code = 15
 	move $a0, $s1				# file descriptor
-	la $a1, successMessageLogging	# string which will be written
+	la $a1, successMessageLogging		# string which will be written
 	la $a2, 32				# specify string length
 	syscall
 	
 	# close file
-	li $v0, 16		# close file, syscall code = 16
-	move $a0, $s1		# file descriptor
+	li $v0, 16				# close file, syscall code = 16
+	move $a0, $s1				# file descriptor
 	syscall
 	
 	#---------------------------------------------#
 	
 	
       	# The program is finished. Exit.
-      	li   $v0, 10          # system call for exit
-      	syscall               # Exit!
+      	li   $v0, 10          			# system call for exit
+      	syscall               			# Exit!
 		
 ###############################################################
 	# Subroutine to print the numbers on one line.
